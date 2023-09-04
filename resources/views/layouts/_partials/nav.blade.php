0db09b6a8677ec1cc9d261ab9837c46940c9dc82
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-09-04 20:12:38 -0300

    navigation

--- resources/views/layouts/_partials/nav.blade.php
+++ resources/views/layouts/_partials/nav.blade.php
@@ -2,14 +2,16 @@
     <nav class="w-full h-[80px] flex items-center justify-center px-[60px] border-zinc-300 gap-5">
 
         <div class="w-[30%] flex">
-            <h1 class="text-2xl font-[800]">JUSTREAD<span class="text-green-700">.</span></h1><span
-                class="text-sm font-semibold text-zinc-600 self-start">WP</span>
+            <a class="flex" href="{{ url('/') }}">
+                <h1 class="text-2xl font-[800]">JUSTREAD<span class="text-green-700">.</span></h1><span
+                    class="text-sm font-semibold text-zinc-600 self-start">WP</span>
+            </a>
         </div>
         <ul class="w-[70%] flex justify-start gap-6 font-semibold text-gray-500">
-            <li class=" cursor-pointer">ABOUT</li>
-            <li class=" cursor-pointer">CATEGORIES</li>
-            <li class=" cursor-pointer">READABILITY</li>
-            <li class=" cursor-pointer">CONTACT</li>
+            <li class=" cursor-pointer"><a href="{{ url('/about') }}">ABOUT</a></li>
+            <li class=" cursor-pointer"><a href="{{ url('/categories') }}">CATEGORIES</a></li>
+            <li class=" cursor-pointer"><a href="{{ url('/readability') }}">READABILITY</a></li>
+            <li class=" cursor-pointer"><a href="{{ url('/contact') }}">CONTACT</a></li>
         </ul>
     </nav>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ReadabilityController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/categories', [CategoriesController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/readability', [ReadabilityController::class, 'index']);
